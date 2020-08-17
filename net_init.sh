40969122110b00dfd69f70537a15997941c69e52
梅田 直希 <han9umeda@umedanoMacBook-Pro.local>
2020-08-17 11:04:24 +0900

    最初のコミット、これからデータ構造をASN連想に変更予定

---
+++ net_init.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+#
+# net_init.sh
+#
+PR_NAME=tmp
+QUAGGA_CONTAINER_IMAGE=quagga_124
+SRX_CONTAINER_IMAGE=srx_511
+
+BNET_ADDRESS_PREFIX=192.168
+PNET_ADDRESS_PREFIX=172.17
+RNET_ADDRESS_PREFIX=172.16.0
+
+# 設定ファイルの読み込み
+
+while read -r line
+do
+	INPUT+=($line)
+done < ${1}
+
+if [ ${INPUT[0]} != AS_Setting ]
+then
+	echo "Syntax error: Keyword \"AS_Setting\" is NOT exist." 1>&2
+	exit 1
+fi
+INPUT=("${INPUT[@]:1}")
+
+while [ ${INPUT[0]} != Peer_info ]
+do
+	AS_NUMBER+=(${INPUT[0]})
+	SECF=${INPUT[1]}
+	if [ $SECF -eq 0 -o $SECF -eq 1 ]
+	then
+		eval SEC_FLAG_AS${INPUT[0]}=$SECF
+	else
+		echo "Syntax error: SEC Flag is 1 or 0." 1>&2
+		exit 1
+	fi
+	INPUT=("${INPUT[@]:2}")
+done
+
+for as in ${AS_NUMBER[@]}
+do
+	eval "echo \$SEC_FLAG_AS$as"
+done
+
+#TODO: Peer_info以下の読み取り
+#TODO: 存在するASであるか確認、一意のPeerであるか確認(重複は削除？)、同じAS同士でないか確認
+
+echo "Including Config file: succeeded"
+
+# ASを作っていく
+for i in $(seq 0 $(expr ${#AS_NUMBER[@]} - 1))
+do
+	ASN=${AS_NUMBER[i]}
+	SECF=${SEC_FLAG[i]}
+	if [ $SECF -eq 1 ]	# BGPsec
+	then
+		echo "docker run -d --name pr_${PR_NAME}_as$ASN ${SRX_CONTAINER_IMAGE}"
+	else			# BGP
+		echo "docker run -d --name pr_${PR_NAME}_as$ASN ${QUAGGA_CONTAINER_IMAGE}"
+	fi
+	echo "docker network create --subnet ${BNET_ADDRESS_PREFIX}.$(expr $i + 1).0/24 pr_${PR_NAME}_bnet_as${ASN}"
+	echo "docker network connect --ip ${BNET_ADDRESS_PREFIX}.$(expr $i + 1).2/24 pr_${PR_NAME}_bnet_as${ASN} pr_${PR_NAME}_as${ASN}"
+	AS_BNET_ADDRESS+=(${BNET_ADDRESS_PREFIX}.$(expr $i + 1).2/24)
+done
+echo ${AS_BNET_ADDRESS[@]}
+
+# AS間接続を構成していく
+
+PEER_ARR1=(72 345)
+PEER_ARR2=(6591 17)
+
+for i in $(seq 0 $(expr ${#PEER_ARR1[@]} - 1))
+do
+	PEER1=${PEER_ARR1[i]}
+	PEER2=${PEER_ARR2[i]}
+	echo "docker network create --subnet ${PNET_ADDRESS_PREFIX}.$(expr $i + 1).0/24 pr_${PR_NAME}_pnet_$(expr $i + 1)"
+	echo "docker network connect --ip ${PNET_ADDRESS_PREFIX}.$(expr $i + 1).2/24 pr_${PR_NAME}_pnet_$(expr $i + 1) pr_${PR_NAME}_as${PEER1}"
+	echo "docker network connect --ip ${PNET_ADDRESS_PREFIX}.$(expr $i + 1).3/24 pr_${PR_NAME}_pnet_$(expr $i + 1) pr_${PR_NAME}_as${PEER2}"
+done
+
+# RPKIを作って、全ASと接続
+echo "docker run -d --name pr_${PR_NAME}_rpki ${SRX_CONTAINER_IMAGE}"
+echo "docker network create --subnet ${RNET_ADDRESS_PREFIX}.0/24 pr_${PR_NAME}_rnet"
+RPKI_ADDRESS=${RNET_ADDRESS_PREFIX}.254
+echo "docker network connect --ip ${RPKI_ADDRESS}/24 pr_${PR_NAME}_rnet pr_${PR_NAME}_rpki"
+for i in $(seq 0 $(expr ${#AS_NUMBER[@]} - 1))
+do
+	ASN=${AS_NUMBER[i]}
+	SECF=${SEC_FLAG[i]}
+	if [ $SECF -eq 1 ]	# BGPsec
+	then
+		echo "docker network connect --ip ${RNET_ADDRESS_PREFIX}.$(expr $i + 2)/24 pr_${PR_NAME}_rnet pr_${PR_NAME}_as${ASN}"
+	fi
+done
+
+# コンテナ内のconfigファイル生成
+# ファイル生成過程で鍵の生成も行う
+
+# 証明書をrpkiへ移動
+
+# コンテナ内でプロセスの立ち上げ
+
