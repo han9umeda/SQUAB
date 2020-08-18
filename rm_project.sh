452cb7e57fb0b8616c910b889c1043ff6853e982
梅田 直希 <han9umeda@umedanoMacBook-Pro.local>
2020-08-19 08:51:46 +0900

    プロジェクト削除において、プロジェクト名が完全一致した場合のみ削除が行われるよう安全に修正

--- rm_project.sh
+++ rm_project.sh
@@ -10,14 +10,14 @@ fi
 
 PR_NAME=$1
 
-CON_LIST=(`docker ps -a --filter "name=pr_$PR_NAME" --format "{{.Names}}"`)
+CON_LIST=(`docker ps -a --filter "name=pr_${PR_NAME}_" --format "{{.Names}}"`)
 for con in ${CON_LIST[@]}
 do
 	docker kill $con
 	docker rm $con
 done
 
-NET_LIST=(`docker network ls --filter="name=pr_$PR_NAME" --format {{.Name}}`)
+NET_LIST=(`docker network ls --filter="name=pr_${PR_NAME}_" --format {{.Name}}`)
 for net in ${NET_LIST[@]}
 do
 	docker network rm $net
