4e3f8401b3a144321dd1e8ccd823087c26c382b6
梅田 直希 <han9umeda@umedanoMacBook-Pro.local>
2021-01-02 12:22:30 +0900

    configファイルに入っている情報を含ませたAS_generatorクラスインスタンスを生成し、アドレスも重複なく割り当たるように実装

--- squab_init.py
+++ squab_init.py
@@ -2,16 +2,45 @@ import sys
 import yaml
 
 class AS_generator:
-  def __init__(self, number):
+  def __init__(self, number, flag, address):
     self._number = number
+    self._flag = flag
+    self._address = address
+  def get_add(self):
+    return self._flag
+
+class Address_detabase:
+  def __init__(self):
+    self.AS_NET_ADDRESS_PREFIX = "191.168."
+    self.PEER_ADDRESS_PREFIX = "171.17."
+    self.RPKI_NET_ADDRESS_PREFIX = "171.16.0."
+
+    self.as_net_i = 2
+    self.peer_address_i = 2
+    self.rpki_net_i = 2
+
+    self.as_net_dict = {}
+    self.peer_address_dict = {}
+    self.rpki_net_dict = {}
+
+  def get_as_net_address(self, as_num):
+
+    if not as_num in self.as_net_dict.keys(): # ASに対応したアドレスがなければ、生成する
+      self.as_net_dict[as_num] = self.AS_NET_ADDRESS_PREFIX + str(self.as_net_i) + ".0/24"
+      self.as_net_i += 1
+
+    return self.as_net_dict[as_num]
+
 
 args = sys.argv
 
 with open(args[1]) as file:
   config = yaml.safe_load(file)
 
-as_generator_dict = {}
-for as_num in config["AS_Setting"].keys():
-  as_generator_dict[as_num] = AS_generator(as_num)
+address_database = Address_detabase()
 
-print(as_generator_dict)
+as_generator_dict = {}
+
+for as_num in config["AS_Setting"].keys():
+  as_generator_dict[as_num] = AS_generator(as_num, config["AS_Setting"][as_num]["flag"], address_database.get_as_net_address(as_num))
+
