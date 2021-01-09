04f5ca39d1f4dc84e24fb00bb9db310e9bd1906b
梅田 直希 <han9umeda@umedanoMacBook-Pro.local>
2021-01-09 18:31:33 +0900

    docker-composeによって全てのIPアドレスが自動で割り振られるようになったので、不要なコードを削除していった結果、Address_databaseクラスを削除し切った

--- squab_init.py
+++ squab_init.py
@@ -10,18 +10,17 @@ import yaml
 import subprocess
 
 class AS_generator:
-  def __init__(self, number, flag, address_database):
+  def __init__(self, number, flag):
     self.number = number
     self.flag = flag
-    self.address_database = address_database
     self.as_network_name = "as_net_" + str(self.number)
 
     self.router_dict = {}
 
-  def make_peer_router_for(self, as_num, address_database, peer_address_flag):
+  def make_peer_router_for(self, as_num):
 
     if not as_num in self.router_dict.keys(): # 対応したルータがなければ、生成する
-      self.router_dict[as_num] = Router_generator(self.number, as_num, address_database, peer_address_flag, self.flag, self.as_network_name)
+      self.router_dict[as_num] = Router_generator(self.number, as_num, self.flag, self.as_network_name)
 
     return self.router_dict[as_num]
 
@@ -71,15 +70,9 @@ class AS_generator:
 
 
 class Router_generator:
-  def __init__(self, on_as, for_as, address_database, peer_address_flag, flag, as_network_name):
+  def __init__(self, on_as, for_as, flag, as_network_name):
     self.on_as = on_as
     self.for_as = for_as
-    self.address_database = address_database
-    self.peer_address = address_database.get_peer_address(on_as, for_as, peer_address_flag)
-    if peer_address_flag == "SMALLER":
-      self.peer_address_opposite = address_database.get_peer_address(on_as, for_as, "BIGGER")
-    elif peer_address_flag == "BIGGER":
-      self.peer_address_opposite = address_database.get_peer_address(on_as, for_as, "SMALLER")
 
     peer_ases = [on_as, for_as]
     peer_ases.sort() # 引数として与えられるAS番号の順番に依存しないようにするため
@@ -98,9 +91,9 @@ class Router_generator:
 
   def get_router_info(self):
     if self.image == "quagga":
-      return {self.router_name: {"image": self.image, "tty": "true", "networks": {self.peer_network_name: {"ipv4_address": self.peer_address}, self.as_network_name: {}}}}
+      return {self.router_name: {"image": self.image, "tty": "true", "networks": {self.peer_network_name: {}, self.as_network_name: {}}}}
     elif self.image == "srx":
-      return {self.router_name: {"image": self.image, "tty": "true", "networks": {self.peer_network_name: {"ipv4_address": self.peer_address}, self.as_network_name: {}, "rnet": {}}}}
+      return {self.router_name: {"image": self.image, "tty": "true", "networks": {self.peer_network_name: {}, self.as_network_name: {}, "rnet": {}}}}
 
   def get_image(self):
     return self.image
@@ -155,45 +148,6 @@ class RPKI_generator:
   def get_rpki_address(self):
     return self.address
 
-class Address_detabase:
-  def __init__(self):
-    self.PEER_ADDRESS_PREFIX = "171.17."
-
-    self.peer_address_i = 2
-
-    self.peer_address_dict = {}
-
-  def get_peer_address(self, peer1, peer2, mode):
-
-    peer_ases = [peer1, peer2]
-    peer_ases.sort() # 引数として与えられるAS番号の順番に依存しないようにするため
-
-    peer_key = str(peer_ases[0]) + "and" + str(peer_ases[1])
-
-    if not peer_key in self.peer_address_dict.keys():
-      self.peer_address_dict[peer_key] = self.PEER_ADDRESS_PREFIX + str(self.peer_address_i) + ".0/24"
-      self.peer_address_i += 1
-
-    if mode == "NET":
-      return self.peer_address_dict[peer_key]
-    elif mode == "SMALLER":
-      return self.peer_address_dict[peer_key][:-5] + ".2"
-    elif mode == "BIGGER":
-      return self.peer_address_dict[peer_key][:-5] + ".3"
-    else:
-      raise ValueError("mode incorrectly!")
-
-  def get_pnet_info(self):
-
-    pnet_info = {}
-    for peer_info in self.peer_address_dict.keys():
-      pnet_info["pnet_" + peer_info] = {"ipam": {"config": [{"subnet": self.peer_address_dict[peer_info]}]}}
-
-    return pnet_info
-
-  def get_rnet_info(self):
-    return {"rnet": {}}
-
 
 def peer_network_name(peer1, peer2):
   peer_ases = [peer1, peer2]
@@ -221,17 +175,15 @@ print("Project name: " + project_name)
 with open(args[1]) as file:
   config = yaml.safe_load(file)
 
-address_database = Address_detabase()
-
 as_generator_dict = {}
 
 for as_num in config["AS_Setting"].keys():
-  as_generator_dict[as_num] = AS_generator(as_num, config["AS_Setting"][as_num]["flag"], address_database)
+  as_generator_dict[as_num] = AS_generator(as_num, config["AS_Setting"][as_num]["flag"])
 
 peer_network_name_list = []
 for peer in config["Peer_info"]:
-  as_generator_dict[peer[0]].make_peer_router_for(peer[1], address_database, "SMALLER")
-  as_generator_dict[peer[1]].make_peer_router_for(peer[0], address_database, "BIGGER")
+  as_generator_dict[peer[0]].make_peer_router_for(peer[1])
+  as_generator_dict[peer[1]].make_peer_router_for(peer[0])
   peer_network_name_list.append(peer_network_name(peer[0], peer[1]))
 
 rpki_generator = RPKI_generator()
@@ -257,8 +209,9 @@ compose_services = {'services': containers_info}
 networks_info = {}
 for as_gen in as_generator_dict.values():
   networks_info.update(as_gen.get_as_net_info())
-networks_info.update(address_database.get_pnet_info())
-networks_info.update(address_database.get_rnet_info())
+for name in peer_network_name_list:
+  networks_info.update({name: {}})
+networks_info.update({"rnet": {}})
 compose_networks = {'networks': networks_info}
 
 with open(compose_file_path, 'w') as f:
