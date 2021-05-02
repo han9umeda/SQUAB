1f548e80ee04c650ce8b958d18f2f9b1ce2d0168
Naoki Umeda <n-umeda@ist.osaka-u.ac.jp>
2021-05-02 20:25:25 +0900

    接続情報について変換を行う部分(後半部分)の実装が完了

--- ripe_to_squab.sh
+++ ripe_to_squab.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-FILENAME=tmp
+OUTPUT_FILE=tmp
 
-echo "AS_Setting:" > $FILENAME
+echo "Make AS list."
+
+echo "AS_Setting:" > $OUTPUT_FILE
 sed -n -e "/path/,/]/{
 	s/^  *//
 	s/,//
@@ -12,4 +14,41 @@ sed -n -e "/path/,/]/{
 }
 " $1 | sort -n | uniq | \
 sed -e "s/[0-9][0-9]*/  &:\n\
-    image: quagga/" >> $FILENAME
+    image: quagga/" >> $OUTPUT_FILE
+
+echo "Make Peer info list."
+
+echo "Peer_info:" >> $OUTPUT_FILE
+
+PREV_AS=""
+CONNECT_INFO_FILE=/tmp/connect
+cat /dev/null > $CONNECT_INFO_FILE
+sed -n -e "/path/,/]/{
+	s/^  *//
+	s/,//
+	p
+}
+" $1 | uniq | while read line
+do
+	if [ "$line" = "\"path\": [" ]
+	then
+		:
+	elif [[ "$line" =~ [0-9][0-9]* ]]
+	then
+		if [ -n "$PREV_AS" ]
+		then
+			if [ $line -lt $PREV_AS ]
+			then
+				echo "  - [$line, $PREV_AS]" >> $CONNECT_INFO_FILE
+			else
+				echo "  - [$PREV_AS, $line]" >> $CONNECT_INFO_FILE
+			fi
+		fi
+		PREV_AS=$line
+	elif [ "$line" = "]" ]
+	then
+		PREV_AS=""
+	fi
+done
+
+cat $CONNECT_INFO_FILE | sort | uniq >> $OUTPUT_FILE
