de51c9a3c4d1a80b00439026c48a546c56ccdae0
Naoki Umeda <n-umeda@ist.osaka-u.ac.jp>
2021-05-03 19:27:46 +0900

    全てのコンテナから bgpdump の結果を取り出す get_all_bgpdump.sh スクリプトを作成

---
+++ get_all_bgpdump.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# SQUAB(Scalable QUagga-based Automated configuration on BGP)
+# get_all_bgpdump.sh
+# input: PROJECT_NAME
+#
+
+if [ $# -ne 1 ]
+then
+	exit 1
+fi
+
+PR_NAME=$1
+
+AS_CON_LIST=(`docker ps -a --filter "name=${PR_NAME}_router" --format "{{.Names}}"`)
+
+for con in ${AS_CON_LIST[@]}
+do
+	echo $con
+	docker exec -i $con bash -c "ls /home/* | grep eth | xargs cat"
+done
