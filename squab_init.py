036c25418651efb768c0da3e85eb5b025fc4439b
Naoki Umeda <n-umeda@ist.osaka-u.ac.jp>
2021-11-10 14:16:38 +0900

    srx AS 内部の接続が正しく設定されていないバグを修正

--- squab_init.py
+++ squab_init.py
@@ -356,10 +356,16 @@ for quagga in quagga_list:
 subprocess.call(["docker", "exec", "-d", project_name + "-rpki-1", "mkdir", "/home/cert"]) # for srx ruoter certificate
 for srx in srx_list:
   rouname = project_name + "-" + srx.get_router_name() + "-1"
+  if len(as_generator_dict[srx.get_on_as_num()].get_router_address_list()) == 1: # There is one router in the AS.
+    neighbor_intra_router_address = ""
+  else: # There is some routers in the AS.
+    neighbor_intra_router_address = as_generator_dict[srx.get_on_as_num()].get_router_address_list()
+    neighbor_intra_router_address.remove(srx.get_intra_as_address())
+    neighbor_intra_router_address = ' '.join(neighbor_intra_router_address)
   subprocess.call(["docker", "exec", "-d", rouname, "/home/cert_setting.sh", rouname, str(srx.get_on_as_num())])
   subprocess.call(["docker", "cp", rouname + ":/var/lib/bgpsec-keys/" + rouname + ".cert", "/tmp"])
   subprocess.call(["docker", "cp", "/tmp/" + rouname + ".cert", project_name + "-rpki-1:/home/cert/"])
-  subprocess.call(["docker", "exec", "-d", rouname, "/home/gen_zebra_bgpd_sec_conf.sh", str(router_index), str(srx.get_on_as_num()), srx.get_as_network_address(), rpki_generator.get_rpki_address(), str(srx.get_for_as_num()), str(srx.get_peer_address_opposite()), rouname])
+  subprocess.call(["docker", "exec", "-d", rouname, "/home/gen_zebra_bgpd_sec_conf.sh", str(router_index), str(srx.get_on_as_num()), srx.get_as_network_address(), rpki_generator.get_rpki_address(), str(srx.get_for_as_num()), str(srx.get_peer_address_opposite()), rouname, neighbor_intra_router_address])
   router_index += 1
 
 print("Starting daemons...")
